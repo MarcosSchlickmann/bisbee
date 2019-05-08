e1838b19916506d44cd9e4e725d691272f20f744
MarcosShlickmann <marcosamichels@gmail.com>
2019-05-08 11:30:30 -0300

    Iniciar confiuguração diretórios flask

--- main.py
+++ main.py
@@ -1,25 +1,7 @@
-from flask import Flask, request, render_template
-from flask_bootstrap import Bootstrap
+import os
+from app import create_app, db
+from app.models import User, Role
+from flask_migrate import Migrate
 
-app = Flask(__name__)
-bootstrap = Bootstrap(app)
-
-@app.route('/')
-def index():
-	return render_template("index.html")
-
-
-@app.route('/user/<nome>')
-def user(nome):
-	return render_template("user.html", name=nome)
-
-
-@app.route('/navegador')
-def navegador():
-	user_agent = request.headers.get("User-Agent")
-	return "<p>{}</p>".format(user_agent)
-
-
-@app.errorhandler(404)
-def page_not_found(e):
-	return render_template("404.html"), 404
+app = create_app()
+# migrate = Migrate(app, db)
